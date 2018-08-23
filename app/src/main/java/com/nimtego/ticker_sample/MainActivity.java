0d13a7b6ef7fc3a91b45333205aa6bb89a5b46da
nimtego <nimtego@mail.ru>
2018-08-23 23:55:22 +0300

    fix clear text

--- app/src/main/java/com/nimtego/ticker_sample/MainActivity.java
+++ app/src/main/java/com/nimtego/ticker_sample/MainActivity.java
@@ -2,14 +2,12 @@ package com.nimtego.ticker_sample;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.EditText;
 
 import com.robinhood.ticker.TickerUtils;
 import com.robinhood.ticker.TickerView;
 
 import java.text.DecimalFormat;
-import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,17 +22,19 @@ public class MainActivity extends AppCompatActivity {
         final EditText summ = findViewById(R.id.editTextSumm);
         tickerView.setText("TEST");
         final Handler handler = new Handler();
-        findViewById(R.id.buttonUp).setOnClickListener((view) -> tickerView.post(new Runnable() {
+        findViewById(R.id.buttonUp).setOnClickListener(view -> tickerView.post(new Runnable() {
             @Override
             public void run() {
                 String debit = String.valueOf(summ.getText());
-                if (debit != null && Float.valueOf(debit) < 1000000000f && Float.valueOf(debit) > 0f) {
+                if (!debit.isEmpty() && Float.valueOf(debit) < 900000000f && Float.valueOf(debit) > 0f) {
                     float second = Float.valueOf(debit) / 576000f;
                     count += second;
                     DecimalFormat df = new DecimalFormat("#.##");
                     tickerView.setText(String.valueOf(df.format(count)));
                     handler.postDelayed(this, 1000);
                 }
+                else
+                    tickerView.setText("");
             }
         }));
     }
